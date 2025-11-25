ef9819f19147055bebac9037b415efe129791819
hhhMyLover <2454988619@qq.com>
2025-11-25 14:22:37 +0800

    Initial commit

---
+++ src/main/java/com/wzh/intelligentcodeapp/IntelligentCodeAppApplication.java
@@ -0,0 +1,13 @@
+package com.wzh.intelligentcodeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentCodeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentCodeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wzh/intelligentcodeapp/IntelligentCodeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzh.intelligentcodeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentCodeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
