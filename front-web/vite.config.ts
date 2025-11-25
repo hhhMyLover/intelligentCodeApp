f80cfa864ccaa0fdd15da64fd2f0a002aa78c46a
hhhMyLover <2454988619@qq.com>
2025-11-25 16:05:49 +0800

    update config front-web

--- front-web/vite.config.ts
+++ front-web/vite.config.ts
@@ -3,12 +3,27 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+//自动导入ui-组件 比如说ant-design-vue  element-plus等
+import Components from 'unplugin-vue-components/vite';
+// 自动导入vue中hook reactive ref等
+import AutoImport from "unplugin-auto-import/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    // vueDevTools(), // 禁用以避免与浏览器扩展冲突
+    AutoImport({
+      //安装两行后你会发现在组件中不用再导入ref，reactive等
+      imports: ['vue', 'vue-router'],
+      dts: "src/auto-import.d.ts",
+      //ant-design-vue
+      resolvers: [AntDesignVueResolver()]
+    }),
+    Components({
+      resolvers: [AntDesignVueResolver({importStyle: false, resolveIcons: true})],
+    }),
   ],
   resolve: {
     alias: {
